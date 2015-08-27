1da122e5c38eb0b36544c9f0cc61347374daf546
protocolsix <protocolsix@gmail.com>
2015-08-26 23:53:19 -0700

    Change URL
    Changing the URL to avoid CORS problems.

--- exercise_one.js
+++ exercise_one.js
@@ -3,7 +3,10 @@ var myfunction = function () {
 	*/
 };
 
-var url = "http://www.google.com";
+// Current San Francisco Weather
+var url = "http://api.openweathermap.org/data/2.5/weather?id=5391959";
+// NOTE: If executing in the browser console, it must be on a http (not https) webpage.
+// Otherwise, an error will probably occur. Don't worry about it.
 
 var oReq = new XMLHttpRequest();
 //something to do with the fuction I made
