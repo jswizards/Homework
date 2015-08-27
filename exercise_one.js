8661d2de8ec1988ee56fc719607896d0ce597a04
Alex Noonan <amnoonann@gmail.com>
2015-08-27 13:50:30 -0700

    Don't look at it yet! It's not done

--- exercise_one.js
+++ exercise_one.js
@@ -1,14 +1,23 @@
-var myfunction = function () {
-	/* still need to figure out what I want it to do.
-	*/
-};
+var oReq = new XMLHttpRequest();
+oReq.onreadystatechange = weather;
 
 // Current San Francisco Weather
 var url = "http://api.openweathermap.org/data/2.5/weather?id=5391959";
 // NOTE: If executing in the browser console, it must be on a http (not https) webpage.
 // Otherwise, an error will probably occur. Don't worry about it.
 
-var oReq = new XMLHttpRequest();
-//something to do with the fuction I made
+
 oRep.open('GET', url);
 oRep.send();
+
+oReq.weather = function () {
+	if (oReq.weather.id === 802) {
+		console.log("Oh snap, it might rain.");
+	}
+};
+
+
+
+
+
+
