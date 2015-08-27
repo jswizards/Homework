7a9c2807808528bee089dc8bdb7ec70e9ec7cf23
Alex Noonan <amnoonann@gmail.com>
2015-08-26 23:22:17 -0700

    First half attempt. Will figure out what I want my function to do exactly tomorrow.

--- exercise_one.js
+++ exercise_one.js
@@ -1,1 +1,11 @@
-test
+var myfunction = function () {
+	/* still need to figure out what I want it to do.
+	*/
+};
+
+var url = "http://www.google.com";
+
+var oReq = new XMLHttpRequest();
+//something to do with the fuction I made
+oRep.open('GET', url);
+oRep.send();
